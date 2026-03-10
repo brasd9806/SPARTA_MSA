f6b0ae963cd541328c322823466322e462c9d764
brasd9806 <ahemswl9806@naver.com>
2026-03-10 22:37:46 +0900

    first commit

---
+++ src/main/java/com/sparta/leehy/lesson/DomainnmApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.leehy.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainnmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainnmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/leehy/lesson/DomainnmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.leehy.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainnmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
